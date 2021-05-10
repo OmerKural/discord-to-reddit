a8ef7d13f5381302d48c70c4e6663d7c352b17f9
omerkural <omer.kural@yahoo.com>
2021-05-10 16:13:28 +0300

    bug fixes

--- main.py
+++ main.py
@@ -30,17 +30,16 @@ with open('karma_data.json', 'r+') as data_file:
 
 # Useful functions
 def add_karma(guild_id, user_id, count):
-    print("adding", count)
     if not guild_id in data:
         data[guild_id] = {}
     if not user_id in data[guild_id]:
         data[guild_id][user_id] = 0
     data[guild_id][user_id] += count
-    with open('karma_data.json', 'w') as data_file:
+    with open('karma_data.json', 'r+') as data_file:
         json.dump(data, data_file)
+    
 
 def subtract_karma(guild_id, user_id, count):
-    print("subtracting", count)
     if not guild_id in data:
         data[guild_id] = {}
     if not user_id in data[guild_id]:
@@ -86,16 +85,6 @@ async def on_message(message):
 # Platinum = +20
 @bot.event
 async def on_raw_reaction_add(payload):
-    with open('karma_data.json', 'r+') as data_file:
-        try:
-            data = json.load(data_file)
-            # Deletes duplicate values ( this is not necessary its just my paranoia! )
-            with open('karma_data.json', 'w') as data_file:
-                json.dump(data, data_file)
-
-        except json.decoder.JSONDecodeError as e:
-            data = {}
-    
     guild_object = bot.get_guild(payload.guild_id)
     channel_object = bot.get_channel(payload.channel_id)
     message_object = await channel_object.fetch_message(payload.message_id)
@@ -103,13 +92,13 @@ async def on_raw_reaction_add(payload):
         if str(payload.emoji) == str(get(guild_object.emojis, name='upvote')):
             add_karma(str(payload.guild_id), str(message_object.author.id), 1)
         if str(payload.emoji) == str(get(guild_object.emojis, name='downvote')):
-            subtract_karma(str(payload.guild_id), message_object.author.id, 1)
+            subtract_karma(str(payload.guild_id), str(message_object.author.id), 1)
         if str(payload.emoji) == str(get(guild_object.emojis, name='reddit_silver')):
-            add_karma(str(payload.guild_id), message_object.author.id, 5)
+            add_karma(str(payload.guild_id), str(message_object.author.id), 5)
         if str(payload.emoji) == str(get(guild_object.emojis, name='reddit_gold')):
-            add_karma(str(payload.guild_id), message_object.author.id, 10)
+            add_karma(str(payload.guild_id), str(message_object.author.id), 10)
         if str(payload.emoji) == str(get(guild_object.emojis, name='reddit_platinum')):
-            add_karma(str(payload.guild_id), message_object.author.id, 20)
+            add_karma(str(payload.guild_id), str(message_object.author.id), 20)
 
         with open('karma_data.json', 'w') as data_file:
             json.dump(data, data_file)
@@ -117,29 +106,20 @@ async def on_raw_reaction_add(payload):
 # [ANTI CHEAT] Prevents users from abusing the upvote, downvote buttons and awards by spamming them. 
 @bot.event
 async def on_raw_reaction_remove(payload):
-    with open('karma_data.json', 'r+') as data_file:
-        try:
-            data = json.load(data_file)
-            # Deletes duplicate values ( this is not necessary its just my paranoia! )
-            with open('karma_data.json', 'w') as data_file:
-                json.dump(data, data_file)
-
-        except json.decoder.JSONDecodeError as e:
-            data = {}
     guild_object = bot.get_guild(payload.guild_id)
     channel_object = bot.get_channel(payload.channel_id)
     message_object = await channel_object.fetch_message(payload.message_id)
     if str(payload.channel_id) == settings['CHANNEL_ID']:
         if str(payload.emoji) == str(get(guild_object.emojis, name='upvote')):
-            subtract_karma(str(payload.guild_id), message_object.author.id, 1)
+            subtract_karma(str(payload.guild_id), str(message_object.author.id), 1)
         if str(payload.emoji) == str(get(guild_object.emojis, name='downvote')):
-            add_karma(str(payload.guild_id), message_object.author.id, 1)
+            add_karma(str(payload.guild_id), str(message_object.author.id), 1)
         if str(payload.emoji) == str(get(guild_object.emojis, name='reddit_silver')):
-            subtract_karma(str(payload.guild_id), message_object.author.id, 5)
+            subtract_karma(str(payload.guild_id), str(message_object.author.id), 5)
         if str(payload.emoji) == str(get(guild_object.emojis, name='reddit_gold')):
-            subtract_karma(str(payload.guild_id), message_object.author.id, 10)
+            subtract_karma(str(payload.guild_id), str(message_object.author.id), 10)
         if str(payload.emoji) == str(get(guild_object.emojis, name='reddit_platinum')):
-            subtract_karma(str(payload.guild_id), message_object.author.id, 20)
+            subtract_karma(str(payload.guild_id), str(message_object.author.id), 20)
 
         with open('karma_data.json', 'w') as data_file:
             json.dump(data, data_file)
