38e920739501078d859439eef0ecbadd451cbe83
omerkural <omer.kural@yahoo.com>
2021-05-10 15:50:25 +0300

    Added award system

--- cogs/KarmaSystem.py
+++ cogs/KarmaSystem.py
@@ -10,23 +10,6 @@ class KarmaSystem(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    # Useful functions
-    def add_award(self, guild_id, user_id, award, count):
-        """award: 'reddit_silver', 'reddit_gold', 'reddit_platinum'"""
-        with open('../karma_data.json') as data_file:
-            data = json.load(data_file)
-        if not guild_id in data:
-            data[guild_id] = {}
-        if not user_id in data[guild_id]:
-            data[guild_id][user_id] = {}
-        if not 'reddit_silver' in data[guild_id][user_id]:
-            data[guild_id][user_id]['reddit_silver'] = 0
-        if not 'reddit_gold' in data[guild_id][user_id]:
-            data[guild_id][user_id]['reddit_gold'] = 0
-        if not 'reddit_platinum' in data[guild_id][user_id]:
-            data[guild_id][user_id]['reddit_platinum'] = 0
-        data[guild_id][user_id][award] += count
-
     # DONE
     @commands.command(name='leaderboard', aliases=['lb'])
     async def leaderboard(self, ctx):
@@ -34,24 +17,20 @@ class KarmaSystem(commands.Cog):
         with open('karma_data.json', 'r') as data_file:
             data = json.load(data_file)
         # Sorts the dict values in descending order.
-        clean_ranking_data = {}
-        for key, value in data[str(ctx.guild.id)].items():
-            for k, v in value.items():
-                clean_ranking_data[key] = v
-        clean_ranking_data = dict(sorted(clean_ranking_data.items(), reverse=True, key=lambda item: item[1]))
+        ranking_data = dict(sorted(data[str(ctx.guild.id)].items(), reverse=True, key=lambda item: item[1]))
         ranking_txt = []
         # Shows at max first ten elements of the rankings.
-        for user_id in (clean_ranking_data[:10] if len(clean_ranking_data) > 10 else clean_ranking_data):
+        for user_id in (ranking_data[:10] if len(ranking_data) > 10 else ranking_data):
             username = await self.bot.fetch_user(user_id)
-            user_rank = list(clean_ranking_data.keys()).index(user_id) + 1
+            user_rank = list(ranking_data.keys()).index(user_id) + 1
             if user_rank == 1:
-                rank_desc = '🥇  ' + str(username) + ' -> ' + str(clean_ranking_data[user_id])
+                rank_desc = '🥇  ' + str(username) + ' -> ' + str(ranking_data[user_id])
             elif user_rank == 2:
-                rank_desc = '🥈  ' + str(username) + ' -> ' + str(clean_ranking_data[user_id])
+                rank_desc = '🥈  ' + str(username) + ' -> ' + str(ranking_data[user_id])
             elif user_rank == 3:
-                rank_desc = '🥉  ' + str(username) + ' -> ' + str(clean_ranking_data[user_id])
+                rank_desc = '🥉  ' + str(username) + ' -> ' + str(ranking_data[user_id])
             else:
-                rank_desc = '**' + str(user_rank) + '.**  ' + str(username) + ' -> ' + str(clean_ranking_data[user_id])
+                rank_desc = '**' + str(user_rank) + '.**  ' + str(username) + ' -> ' + str(ranking_data[user_id])
             ranking_txt.append(str(rank_desc))
         embed = discord.Embed(title="Karma Leaderboard 🏆", description='\n'.join(ranking_txt), color=0xff5600)
         await ctx.send(embed=embed)
@@ -65,24 +44,34 @@ class KarmaSystem(commands.Cog):
         server_id = str(ctx.guild.id)
         author_id = str(ctx.author.id)
         try:
-            await ctx.send(f"You have **{data[server_id][author_id]['karma']}** karma.")
+            await ctx.send(f"You have **{data[server_id][author_id]}** karma.")
         except KeyError:
-            data[server_id][author_id]['karma'] = 0
-            await ctx.send(f"You have **{data[server_id][author_id]['karma']}** karma.")
+            data[server_id][author_id] = 0
+            await ctx.send(f"You have **{data[server_id][author_id]}** karma.")
 
-    # todo buy award
-    @commands.command(name='buy_award')
-    async def buy_award(self, ctx, award):
-        "Awards a user's post by substracting karmas from your karma points. (Silver=10, Gold=25, Platinum=50)"
+    # Condition: User must have 25 karma points to use silver, 50 for gold and 100 for platinum.
+    @commands.command(name='award')
+    async def award(self, ctx, award_name):
+        """User must have 25 karma points to award a silver, 50 for gold and 100 for platinum. Reply to the message you want to award.
+         \nAward name can be: silver, gold, platinum"""
+        with open('karma_data.json', 'r') as data_file:
+            data = json.load(data_file)
+            if award_name.lower() == 'silver' and data[str(ctx.guild.id)][str(ctx.author.id)] >= 25:
+                await ctx.message.reference.resolved.add_reaction(get(ctx.guild.emojis, name='reddit_silver'))
+            elif award_name.lower() == 'gold' and data[str(ctx.guild.id)][str(ctx.author.id)] >= 50:
+                await ctx.message.reference.resolved.add_reaction(get(ctx.guild.emojis, name='reddit_gold'))
+            elif award_name.lower() == 'platinum' and data[str(ctx.guild.id)][str(ctx.author.id)] >= 100:
+                await ctx.message.reference.resolved.add_reaction(get(ctx.guild.emojis, name='reddit_platinum'))
+        
+    
+    @commands.Cog.listener()
+    async def on_raw_reaction_add(self, payload):
+        guild_object = self.bot.get_guild(payload.guild_id)
         # Adds custom emojis if it hasn't been done already.
         for filename in os.listdir('./emojis/awards'):
-            if filename[:-4] not in [emoji.name for emoji in ctx.guild.emojis]:
+            if filename[:-4] not in [emoji.name for emoji in guild_object.emojis]:
                 with open(f'./emojis/awards/{filename}', 'rb') as image:
-                    await ctx.guild.create_custom_emoji(name=filename[:-4], image=image.read(), roles=[get(ctx.guild.roles, name='shitpost2reddit')])
-
-        # todo market UI
-        # Adds the awards to users wallet 'karma_data.json'. (data[guild_id][user_id][award])
-        #self.add_award(ctx.guild.id, ctx.user.id, )
+                    await guild_object.create_custom_emoji(name=filename[:-4], image=image.read(), roles=[get(guild_object.roles, name='discord2reddit')])
 
 
 def setup(bot):
